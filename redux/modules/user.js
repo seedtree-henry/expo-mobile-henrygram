760a3d24f38b398280347aa265eeb8b46ba47958
Henry KIM <seedtree.henry@gmail.com>
2018-07-19 03:20:20 +1000

    persisting the redux preparation - configureStore.js

---
+++ redux/modules/user.js
@@ -0,0 +1,30 @@
+// Import 
+
+// Actions
+
+// Action Creators
+
+// API Actions
+
+// Initial State
+
+const intialState = {
+  isLoggedIn: false
+}
+
+// Reducer
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+// Reducer Functions
+
+// Exports
+
+// Default Reducer Export
+
+export default reducer;
