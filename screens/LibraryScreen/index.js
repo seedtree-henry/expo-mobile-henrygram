b4f0a9e0784f7ffc0e1d697dd863e4488fcd1ebe
Henry KIM <seedtree.henry@gmail.com>
2018-07-27 03:55:40 +1000

    pick an image from library

--- screens/LibraryScreen/index.js
+++ screens/LibraryScreen/index.js
@@ -1,10 +1,32 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { Component } from "react";
+import { CameraRoll } from "react-native";
+import LibraryScreen from "./presenter";
+class Container extends Component {
+  state = {
+    photo: null,
+    pickedPhoto: null
+  };
 
-const LibraryScreen = props => (
-  <View>
-    <Text>Libraray</Text>
-  </View>
-);
+  componentWillMount = async () => {
+    const cameraPhotos = await CameraRoll.getPhotos({
+      first: 20,
+      groupTypes: "SavedPhotos",
+      assetType: "Photos"
+    });
+    this.setState({
+      photos: cameraPhotos.edges,
+      pickedPhoto: cameraPhotos.edges[0]
+    });
+  };
 
-export default LibraryScreen;
+  render() {
+    return <LibraryScreen {...this.state} pickPhoto={this._pickPhoto} />;
+  }
+  _pickPhoto = photo => {
+    this.setState({
+      pickedPhoto: photo
+    });
+  };
+}
+
+export default Container;
